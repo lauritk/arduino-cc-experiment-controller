37851713fc66934b9ba17c7af4939a61277d1187
Lauri Kantola <contact@laurikantola.info>
2018-08-07 13:48:30 +0300

    Initial commit

---
+++ cc-experiment-controller/cc-experiment-controller.ino
@@ -0,0 +1,19 @@
+// 1st program parameters
+unsigned long const CS = 50;
+unsigned long const US = 20;
+unsigned long const ISI = 300;
+unsigned long const ITI_LOW = 30;
+unsigned long const ITI_HIGH = 100;
+int const TRIALS = 60;
+
+// 2nd program parameters
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
