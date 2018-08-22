3790f019d4fbd26360e3191d45fbdaebd8471fa7
Lauri Kantola <contact@laurikantola.info>
2018-08-22 15:43:59 +0300

    Update

--- cc-experiment-controller/cc-experiment-controller.ino
+++ cc-experiment-controller/cc-experiment-controller.ino
@@ -10,7 +10,7 @@ unsigned long const ITI_HIGH_A = 30000;
 unsigned long const CS_WIDTH_B = CS_WIDTH_A;
 unsigned long const CS_TONE_B = CS_TONE_A;
 unsigned long const US_WIDTH_B = US_WIDTH_A;
-unsigned long const ISI_B = ISI_A;
+unsigned long const ISI_B = ISI_A + CS_WIDTH_A;
 unsigned long const ITI_LOW_B = ITI_LOW_A;
 unsigned long const ITI_HIGH_B = ITI_HIGH_A;
 
@@ -19,7 +19,8 @@ int const TRIALS = 60;
 
 int const CS_PIN = 3, CS_TONE_PIN = 8, US_PIN = 5, SWITCH_PIN = 6, START_PIN = 4;
 
-// START_PIN can be substitute with reset switch
+// CS_TONE_PIN send only square wave. Button on the device may blink in "wrong" time.
+// CS_TONE_PIN can be changed to regular pulse.
 
 
 
@@ -52,7 +53,7 @@ void loop() {
       }
   }
   int program_num = !digitalRead(SWITCH_PIN);
-  Serial.println("Trial number: " + String(trial_num) + ", " + String(millis()));
+  // Serial.println("Trial number: " + String(trial_num) + ", " + String(millis()));
   program(program_num);
   if (trial_num > 60) {
     while (true) {
@@ -72,10 +73,10 @@ void loop() {
 
 void program(int program_num) {
   if (!program_num) {
-    Serial.println("Trace trial start, " + String(millis()));
+    // Serial.println("Trace trial start, " + String(millis()));
     trace_cc();
   } else {
-    Serial.println("Delay trial start, " +String(millis()));
+    // Serial.println("Delay trial start, " +String(millis()));
     delay_cc();
   }
 }
@@ -85,7 +86,7 @@ void wait_rand(String name, unsigned long low, unsigned long high) {
 }
 
 void print_time(String name, unsigned long start, unsigned long end) {
-  Serial.println(name + ", " + String(start) + ", " + String(end));
+  // Serial.println(name + ", " + String(start) + ", " + String(end));
 }
 
 void wait(String name, unsigned long width) {
@@ -115,17 +116,14 @@ void trace_cc() {
   pulse("CS_A", CS_PIN, CS_WIDTH_A);
   wait("ISI_A", ISI_A);
   pulse("US_A", US_PIN, US_WIDTH_A);
-  pulse_tone("CS_TONE_A", CS_TONE_PIN, CS_TONE_A, CS_WIDTH_A);
 }
 
-// 2nd program
+// 2st program
 void delay_cc() {
   wait_rand("ITI_B", ITI_LOW_B, ITI_HIGH_B);
-  Serial.println("CS_B, ISI_B Start, " + String(millis()));
   digitalWrite(CS_PIN, HIGH);
-  wait("ISI_B", CS_WIDTH_B + ISI_B - US_WIDTH_B);
+  wait("ISI_B", ISI_B);
   pulse("US_B", US_PIN, US_WIDTH_B);
-  Serial.println("CS_B, ISI_B End, " + String(millis()));
   digitalWrite(CS_PIN, LOW);
 }
 
